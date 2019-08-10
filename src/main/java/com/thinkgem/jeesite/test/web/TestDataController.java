3eccaf3e9e29c609a5e078b882f238ba8eff356e
李伟 <lnlw521@163.com>
2019-08-10 22:03:08 +0800

    Update TestDataController.java

--- src/main/java/com/thinkgem/jeesite/test/web/TestDataController.java
+++ src/main/java/com/thinkgem/jeesite/test/web/TestDataController.java
@@ -3,9 +3,12 @@
  */
 package com.thinkgem.jeesite.test.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.thinkgem.jeesite.common.config.Global;
+import com.thinkgem.jeesite.common.persistence.Page;
+import com.thinkgem.jeesite.common.utils.StringUtils;
+import com.thinkgem.jeesite.common.web.BaseController;
+import com.thinkgem.jeesite.test.entity.TestData;
+import com.thinkgem.jeesite.test.service.TestDataService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,12 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.thinkgem.jeesite.common.config.Global;
-import com.thinkgem.jeesite.common.persistence.Page;
-import com.thinkgem.jeesite.common.web.BaseController;
-import com.thinkgem.jeesite.common.utils.StringUtils;
-import com.thinkgem.jeesite.test.entity.TestData;
-import com.thinkgem.jeesite.test.service.TestDataService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 单表生成Controller
